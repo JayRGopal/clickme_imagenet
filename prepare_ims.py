cca78555af9ddbe33bdc196819e363ad6e313abf
grassknoted <grassknoted@gmail.com>
2022-12-12 18:47:09 -0500

    os mask issue 2

--- prepare_ims.py
+++ prepare_ims.py
@@ -94,8 +94,6 @@ try:
     
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
-    if not os.path.exists(validation_dir):
-        os.makedirs(validation_dir)
     if not os.path.exists('db_dump'):
         os.makedirs('db_dump')
 
